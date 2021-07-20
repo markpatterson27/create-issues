953cbf10682cd302a49d8f64d836df1d0132f9f7
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-07-20 22:05:01 +0100

    added test settings

---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+Object.assign(process.env, {
+    GITHUB_EVENT_PATH: path.join(__dirname, 'fixtures', 'event.json'),
+    GITHUB_WORKSPACE: path.join(__dirname, '..'),
+    GITHUB_ACTION: 'test',
+    GITHUB_ACTOR: 'jest tests'
+});
